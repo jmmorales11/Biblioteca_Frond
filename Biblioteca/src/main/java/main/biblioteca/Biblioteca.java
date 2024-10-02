c8ac3d865d115bd47fd47fed26558b531c5cb1da
Marley <jeimymorales234@gmail.com>
2024-10-02 10:31:45 -0500

    Login

---
+++ Biblioteca/src/main/java/main/biblioteca/Biblioteca.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main.biblioteca;
+
+import view.LogIn;
+
+/**
+ *
+ * @author ASUS TUF A15
+ */
+public class Biblioteca {
+
+    public static void main(String[] args) {
+        LogIn loginWindow = new LogIn();
+        
+        // Hacer visible la ventana
+        loginWindow.setVisible(true);
+    }
+}
